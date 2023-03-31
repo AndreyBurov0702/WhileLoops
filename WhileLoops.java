07abdbe6219f1a76dcecf4dee7e23d93aeb5ccdc
AndreyBurov0702 <129365108+AndreyBurov0702@users.noreply.github.com>
2023-03-31 20:32:04 +0500

    Add files via upload

---
+++ WhileLoops.java
@@ -0,0 +1,9 @@
+public class WhileLoops {
+    public static void main(String[] args) {
+        int value = 0;
+        while(value<5){ //выполняется пока условие дает значение true
+            System.out.println("Hello "+value);
+            value = value+1;
+        }
+    }
+}
